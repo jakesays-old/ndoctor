fbbcd42673ab83a7bb1906c5de0184a313c46375
jibedoubleve <jibedoubleve@b436941e-8c21-5fc4-0da6-ce95ff6dc6cd>
2011-08-18 17:24:37 +0000

    setup repository

---
+++ trunk/tools/CleanSolution.cmd
@@ -0,0 +1,8 @@
+@echo off
+set cmd ="c:\Program Files\Python 3.1.1\python.exe"
+set script="C:\Projects\nDoctor\tools\CleanSolution.py"
+set args="C:\Projects\nDoctor"
+
+%cmd% %script% %args%
+
+pause
