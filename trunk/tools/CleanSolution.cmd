8a11fb6ab1d833f2210b9358bac4e7a94a116ad3
jibedoubleve <jibedoubleve@b436941e-8c21-5fc4-0da6-ce95ff6dc6cd>
2012-06-19 19:21:52 +0000

    * Update Mvvm-core * Add Colourisation VS settings * Update release script

--- trunk/tools/CleanSolution.cmd
+++ trunk/tools/CleanSolution.cmd
@@ -1,7 +1,7 @@
 @echo off
 set cmd ="C:\Python32\python.exe"
-set script="C:\Project\nDoctor-3.0\tools\CleanSolution.py"
-set args="C:\Project\nDoctor-3.0\src"
+set script="C:\Projects\ndoctor\tools\CleanSolution.py"
+set args="C:\Projects\ndoctor\src"
 
 %cmd% %script% %args%
 
