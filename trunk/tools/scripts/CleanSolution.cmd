74c99f33a5dbe82108731daf2175b694274d75e3
jibedoubleve <jibedoubleve@b436941e-8c21-5fc4-0da6-ce95ff6dc6cd>
2012-09-24 15:49:22 +0000

    Replaced the Python cleaning script with a PowerShell script

--- trunk/tools/scripts/CleanSolution.cmd
+++
@@ -1,8 +0,0 @@
-@echo off
-set cmd ="C:\Python32\python.exe"
-set script="C:\Projects\ndoctor\tools\scripts\CleanSolution.py"
-set args="C:\Projects\ndoctor\src"
-
-%cmd% %script% %args%
-
-pause
---
+++ trunk/tools/scripts/CleanSolution.ps1
@@ -0,0 +1,34 @@
+﻿param($root = "c:\Projects\ndoctor\src\")
+$items = get-childitem $root -Recurse -Force
+[int]$dirCount = 0
+[int]$fileCount = 0
+
+clear
+
+foreach($item in $items)
+{
+	$name = $item.Name.ToLower()
+	
+	if($item.Attributes -eq "Directory")
+	{		
+		if($name -eq "bin" -or $name -eq "obj" -or $name -eq "release" -or $name -eq "debug")
+		{
+			Write-Host "Deleting"$item.FullName -ForegroundColor Green
+			Remove-Item $item.FullName -Force -Recurse
+			$dirCount++
+		}
+	}
+	else
+	{
+		if($item.Extension -eq ".suo" -or $item.Extension -eq ".user")
+		{
+			Write-Host "Deleting"$item.FullName -ForegroundColor Cyan
+			Remove-Item $item.FullName -Force
+			$fileCount++
+		}
+	}
+}
+	""
+	"Cleaning done:"
+	"$dirCount folder(s) deleted"
+	"$fileCount file(s) deleted"
