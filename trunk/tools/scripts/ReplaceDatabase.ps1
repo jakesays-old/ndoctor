172b4e56baab5c061d4c5b4ce9d0b2e3ff83c172
jibedoubleve <jibedoubleve@b436941e-8c21-5fc4-0da6-ce95ff6dc6cd>
2012-11-29 17:21:21 +0000

    update tools and documentation

--- trunk/tools/scripts/ReplaceDatabase.ps1
+++ trunk/tools/scripts/ReplaceDatabase.ps1
@@ -4,4 +4,6 @@ $path = 'C:\Users\jibedoubleve\AppData\Roaming\Probel\nDoctor\'
 $backup = $path + 'Database - Copy.db'
 $database = $path + 'Database.db'
 
+Write-Host 'Replacing current database with the backup...' -NoNewline
 Copy-Item -Path $backup -Destination $database -Force 
+Write-Host 'Done'
