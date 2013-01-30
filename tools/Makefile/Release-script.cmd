734875480c453b3ad7f0734f2f1ad32af4639645
jibedoubleve <jibedoubleve@b436941e-8c21-5fc4-0da6-ce95ff6dc6cd>
2013-01-30 20:08:22 +0000

    Update script launchers

--- tools/Makefile/Release-script.cmd
+++ tools/Makefile/Release-script.cmd
@@ -2,5 +2,5 @@
 setx NEST C:\nDoctor-Release\Nest\nDoctor
 echo %NEST% 
 
-nant -buildfile:nDoctor.build.xml -D:build-mode=release -D:release-dir="c:\nDoctor-Release" Clean-Up
+nant -buildfile:nDoctor.build.xml -D:build-mode=release -D:release-dir=c:\nDoctor-Release Clean-Up
 pause
