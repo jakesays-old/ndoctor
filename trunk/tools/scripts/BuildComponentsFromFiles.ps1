1b3bf1a1706e2f72cd37fa49e2f690deeaf92bbb
jibedoubleve <jibedoubleve@b436941e-8c21-5fc4-0da6-ce95ff6dc6cd>
2013-01-30 19:51:14 +0000

    Fix error in script

--- trunk/tools/scripts/BuildComponentsFromFiles.ps1
+++ trunk/tools/scripts/BuildComponentsFromFiles.ps1
@@ -3,9 +3,9 @@ echo "Fill Setup.wxs with all the components that contain the nDoctor files"
 ################################################################################
 clear
 $cmdPath = Split-Path($MyInvocation.MyCommand.Path)
-$root = "$cmdPath\..\..\src\Setup\"
+$root = "$cmdPath\..\..\src\Setup"
 $token = '<!--<REPLACEMENT>-->'
-$text = Get-Content $root'Files.wxs'
+$text = Get-Content $root\'Files.wxs'
 echo "."
 echo "========================================================================"
 echo "cmd path: $cmdpath"
@@ -23,19 +23,19 @@ foreach( $m in $matches)
 	$str +=[string]::Format("{0}{1}", $line, [Environment]::NewLine)
 }
 
-$destinationContent = Get-Content $root"Setup.wxs.template"
+$destinationContent = Get-Content $root\"Setup.wxs.template"
 $newText = $destinationContent -replace $token, $str
 ################################################################################
 echo "Add license file"
 ################################################################################
 Write-Host "Copy file: $root\license.rtf"
 Write-Host "       to: $env:NEST"
-cp "$root\license.rtf" $env:NEST
+cp $root\license.rtf $env:NEST
 ################################################################################
 echo "Build the new file"
 ################################################################################
-Write-Host "Write content into $root$template..." -NoNewline
-Set-Content -Path $root"Setup.wxs" -Value $newText
+Write-Host "Write content into $root\$template..." -NoNewline
+Set-Content -Path $root\"Setup.wxs" -Value $newText
 Write-Host " Done"
 ################################################################################
 echo "Get application version"
@@ -47,8 +47,8 @@ $versionPattern = '(?m:)\d+\.\d+\.\d+'
 
 $version = Select-String -path $versionPath -Pattern $versionPattern -AllMatches | % { $_.Matches } | % { $_.Value } 
 
-$setupwxs = Get-Content $root'Setup.wxs'
+$setupwxs = Get-Content $root\'Setup.wxs'
 $newSetupWxs = $setupwxs -replace '\sVersion.?=.?"\d+\.\d+\.\d+\.\d+"', " Version=`"$version`""
 
-Set-Content -Path $root'Setup.wxs' -Value $newSetupWxs
+Set-Content -Path $root\'Setup.wxs' -Value $newSetupWxs
 Write-Host "Done"
