b9da34f2eec82be78fe751c9eb2b79927b261c58
jibedoubleve <jibedoubleve@b436941e-8c21-5fc4-0da6-ce95ff6dc6cd>
2013-01-30 16:37:01 +0000

    update issue 180  * Creating a debug script  * Updated the nAnt script to be more flexible

--- trunk/tools/Makefile/Release-script.cmd
+++ trunk/tools/Makefile/Release-script.cmd
@@ -1,3 +1,4 @@
 @echo off
-nant -buildfile:nDoctor.build.xml -D:build-mode=release Release
+set nest="C:\nDoctor-Release\Nest\nDoctor"
+nant -buildfile:nDoctor.build.xml -D:build-mode=release -D:release-dir="c:\nDoctor-Release" Clean-Up
 pause
