e8053a4aa46a460bb427e2b1109136ed862d562b
jibedoubleve <jibedoubleve@b436941e-8c21-5fc4-0da6-ce95ff6dc6cd>
2013-01-30 19:05:44 +0000

    Updated installation script

--- tools/Makefile/Debug-script.cmd
+++
@@ -1,5 +0,0 @@
-@echo off
-setx nest "C:\Projects\ndoctor\src\View\Probel.NDoctor.View.Core\bin\Debug"
-echo %nest%
-nant -buildfile:nDoctor.build.xml -D:build-mode=debug -D:release-dir=C:\Projects\ndoctor -D:code-dir=C:\Projects\ndoctor Clean-Up
-pause
